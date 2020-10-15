560106a85d897c18d7097d443efd8755ffcfb443
Connor <70128040+cspence97@users.noreply.github.com>
2020-10-14 20:23:32 -0600

    Update findWords.py

--- findWords.py
+++ findWords.py
@@ -1,53 +1,81 @@
 def load_words():
-    with open('list58k.txt') as word_file: # 58,000 most common words in english language 
+    word_list = "list_of_58k_words.txt"  # delete later
+    while True:
+        break  # delete later
+        prompt = input("Use 58k words or 479k?: ").lower()
+        if prompt == "58" or prompt == "58k":
+            word_list = "list_of_58k_words.txt"
+            break
+        elif prompt == "479" or prompt == "479k":
+            word_list = "words_dictionary.json"
+            break
+
+    with open(word_list) as word_file:  # 58,000 most common words in english language
         valid_words = set(word_file.read().split())
     return valid_words
 
-theDict = load_words() # list of words in the english dictionary
 
-mystring = 'spongebob'
+theDict = load_words()  # list of words in the english dictionary
 
-validWords = [] # output of program
+mystring = input("Enter some phrase to rearrange: ")
 
-stringDict = {} # keys = letters in mystring, values = multiplicity of letter 
+validWords = []  # output of program
 
+stringDict = {}  # keys = letters in mystring, values = multiplicity of letter
+wordList = []  # valid words
 # Word is valid if multiplicity of each letter does not exceed that in string
-# and letter in word contained in string 
+# and letter in word contained in string
 
 # make dictionary of characters in the mystring
-for char in mystring.strip(): # strip removes spaces 
+for char in mystring.strip():  # strip removes spaces
     if char not in stringDict.keys():
         stringDict[char] = 1
     else:
         stringDict[char] += 1
 
-for word in theDict:
-    wordDict = {} # keys = letters in word, values = multiplicity of letter 
 
-    # make dictionary of characters in the word
-    for char in word:
-        if char not in wordDict.keys():
-            wordDict[char] = 1
+def check_dict_words():
+    for word in theDict:
+        if check_word(word):
+            validWords.append(word)
+
+
+def check_word(word):
+    wDict = {}
+    for w in word.strip():
+        if w not in wDict.keys():
+            wDict[w] = 1
         else:
-            wordDict[char] += 1
-            
-    valid = True # assume word is contained mystring 
-    
-    # if word is longer than mystring, word does not appear
-    if len(word) > len(mystring.strip()):
-        valid = False
+            wDict[w] += 1
+    if len(word) > len(mystring):
+        return False
     else:
-        for char in word:
-            # if word contains character not in mystring, word does not appear
-            if char not in mystring:
-                valid = False
-                break
-            # if word contains a character appearing more times than in mystring, word does not appear
-            elif wordDict[char] > stringDict[char]:
-                valid = False
-                break 
-        
-    if valid == True:
-        validWords.append(word)
-        print(word)
+        for c in word:
+            if c not in stringDict.keys():
+                return False
+            elif wDict[c] > stringDict[c] != 0:
+                return False
+    return True
 
+
+def checkMultiple():
+    for word in validWords:
+        wordList.append(word)
+        while len(word) < len(mystring):
+            breakit = False
+            for w in validWords:
+                if check_word(word + w):
+                    word = word + " " + w
+                    wordList.append(word)
+
+                elif w == validWords[-1]:
+                    breakit = True
+            if breakit:  # needed to break while loop from for loop too lazy to do cleaner
+                break
+
+
+check_dict_words()
+validWords.sort(key=len)
+checkMultiple()
+for w in wordList:
+    print(w)
